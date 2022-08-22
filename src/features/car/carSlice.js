08a70c66712b62b70ee3e35c8cf30dedc2def05f
HARSH VISHWAKARMA <harshvishwakarma111@gmail.com>
2022-08-22 10:11:07 +0530

    Tesla clone project done

---
+++ src/features/car/carSlice.js
@@ -0,0 +1,12 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const intialState = {
+  cars: ["Model S", "Model 3", "Model X", "Model Y"],
+};
+const carSlice = createSlice({
+  name: "car",
+  intialState,
+  reducers: {},
+});
+export const selectCars = (state) => state.car.cars;
+export default carSlice.reducers;
